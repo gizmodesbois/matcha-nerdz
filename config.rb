5705f158a420d77ff837ff5ecac07b2a38fcbc73
Simon Henrotte <henrotte.simon@gmail.com>
2016-02-24 08:32:35 +0100

    Adds setup for GitHub Pages deployment

--- config.rb
+++ config.rb
@@ -97,8 +97,14 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
+  deploy.build_before = true
 end
---
+++ source/posts/2016-my-awesome-3rd-article.html.markdown
@@ -0,0 +1,6 @@
+---
+title: My awesome 3rd article
+date: 2016-02-24 06:51 UTC
+tags:
+---
+
